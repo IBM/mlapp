2290945bb6a89324c08d6d1bdde405142db528e7
tgalula <16176167+tgalula@users.noreply.github.com>
2021-03-11 13:13:15 +0200

    update

--- mlapp/integrations/aml/utils/runconfig.py
+++ mlapp/integrations/aml/utils/runconfig.py
@@ -1,4 +1,6 @@
 from azureml.core.runconfig import RunConfiguration
+from azureml.core.environment import DEFAULT_CPU_IMAGE
+from azureml.core import Environment
 
 
 def create_runconfig(aml_compute, env=None):
@@ -11,19 +13,15 @@ def create_runconfig(aml_compute, env=None):
     if env is not None:
         aml_run_config.environment = env
     else:
+        aml_run_config.environment = Environment.from_pip_requirements(name='mlapp', file_path='requirements.txt')
+
         # Enable Docker
         aml_run_config.environment.docker.enabled = True
 
         # Set Docker base image to the default CPU-based image
-        aml_run_config.environment.docker.base_image = "mcr.microsoft.com/azureml/base"
+        aml_run_config.environment.docker.base_image = DEFAULT_CPU_IMAGE
 
         # Use conda_dependencies.yml to create a conda environment in the Docker image for execution
-        aml_run_config.environment.python.user_managed_dependencies = False
+        aml_run_config.environment.python.user_managed_dependencies = True
 
     return aml_run_config
-
-
-
-
-
-

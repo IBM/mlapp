3f84d47f3e4d6fc6ce0ae7e0fbd5c316c7e2cbdb
tgalula <16176167+tgalula@users.noreply.github.com>
2021-03-14 21:03:29 +0200

    updated helper cli desc

--- mlapp/integrations/aml/cli_help.py
+++ mlapp/integrations/aml/cli_help.py
@@ -1,25 +1,25 @@
 cli_aml_help = {
     "setup": \
-'''Usgae:
+'''Usage:
 
 `mlapp aml setup` - setup Azure Machine Learning deployment files in your project.
 ''',
     "publish_pipeline": \
-'''Usgae:
+'''Usage:
 
 `mlapp aml publish-pipeline [PIPELINE_NAME] [COMPUTE_TRAGET]` - set the pipeline name and the correct compute target.
 
 for further options look on the options section below.
 ''',
     "deploy_model": \
-'''Usgae:
+'''Usage:
 
-`mlapp aml deploy-model [ASSET_NAME]` - set your asset name.
+`mlapp aml deploy-model [EXPERIMENT_NAME] [ASSET_NAME]` - set your asset name.
 
 for further options look on the options section below.
 ''',
     "add-mlapp-env": \
-'''Usgae:
+'''Usage:
 
 `mlapp aml create-mlapp-env` - create a new python environment containing the ML App library in AzureML. 
 

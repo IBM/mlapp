97f7251585ca2e56c058398e9d42fb0c57d4d39c
tgalula <16176167+tgalula@users.noreply.github.com>
2021-03-24 17:56:05 +0200

    added fix for aml deployment

--- mlapp/integrations/aml/generated_files/aml_deployment.py
+++ mlapp/integrations/aml/generated_files/aml_deployment.py
@@ -1,7 +1,8 @@
 import os
 import traceback
-
 import pandas as pd
+from config import settings
+from mlapp import MLApp
 from mlapp.integrations.aml.utils.deploy import preprocess_deployment, get_predictions_path, run_config, \
     insert_model_id
 from mlapp.integrations.aml.utils.run_class import load_config_from_string
@@ -10,7 +11,7 @@ from mlapp.integrations.aml.utils.run_class import load_config_from_string
 def init():
     global run_id
     model_path = os.path.join(os.getcwd(), os.getenv('AZUREML_MODEL_DIR'))
-    os.chdir('mlapp_test')
+    os.chdir('<LOCAL_ROOT_DIRECTORY_NAME>')
     output_dir = os.path.join(os.getcwd(), 'output')
     os.makedirs(output_dir, exist_ok=True)
     run_id = preprocess_deployment(model_path)
@@ -25,7 +26,8 @@ def run(raw_config):
         insert_model_id(config, run_id)
 
         # running config
-        ids = run_config(config)
+        mlapp = MLApp(settings)
+        _, ids, outputs = mlapp.run_flow_from_config(config)
 
         # loading predictions and returning it
         file_path = get_predictions_path(ids)
